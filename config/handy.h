4b868fe6deeb707a93f6f7b80ccfaf1f45692c20
Fogelklou, Christopher (H) <chris.fogelklou@polestar.com>
2018-11-14 11:27:00 +0100

    Fix MSVC build.

---
+++ config/handy.h
@@ -0,0 +1,15 @@
+#ifndef LOCALHANDY_H__
+#define LOCALHANDY_H__
+
+#include <string.h>
+#include <stdint.h>
+#define MIN(x,y) (((x) < (y)) ? (x) : (y))
+
+static inline void mem_clean(volatile void *v, size_t len){
+  if (len){
+    memset((void *)v, 0, len);
+    (void) *((volatile uint8_t *)v);
+  }
+}
+
+#endif
